fbd85eeff044e1d27100d671d445678bfb59a36a
Ayenem <moub.ahmed@hotmail.com>
2024-01-04 13:30:08 -0500

    force_word_ids with disjunctive constraint + better autodoc

--- token_healing.py
+++ token_healing.py
@@ -1,6 +1,6 @@
 from itertools import takewhile
+
 from transformers.generation import MaxLengthCriteria
-from transformers.generation import PrefixConstrainedLogitsProcessor as AllowedToks
 from torch import IntTensor
 from pygtrie import CharTrie
 
@@ -14,26 +14,26 @@ class TokenBoundaryHealer:
     def __call__(self, prompt: str) -> str:
         prompt_ids = self.encode(prompt, return_tensors='pt').cuda()
         if alts := self.get_tail_alts(prompt_ids):
-            healed_ids = self.regen_toks(prompt_ids[:, : -len(alts) or None], alts)
+            trimmed_ids = prompt_ids[:, : -len(alts) or None]
+            healed_ids = self.regenerate_tokens(trimmed_ids, alts)
             prompt = self.decode(healed_ids.squeeze(), skip_special_tokens=True)
         return prompt
 
     def get_tail_alts(self, prompt_ids: IntTensor) -> IntTensor:
         prompt_toks = [*map(self.decode, prompt_ids.squeeze())]
-
-        tail_toks_extensions = ( # ids of e.g. ['.', ':'] -> [['.', '. '], [':', '://']]
-            self.vocab_trie.values(prefix=tail_tok.lstrip()) for tail_tok in reversed(prompt_toks)
-        ) # querying contiguous tail tokens for alternative tokens
+        tail_toks_extensions = (
+            self.vocab_trie.values(prefix=tail_tok.lstrip())
+            for tail_tok in reversed(prompt_toks)
+        ) # retrieving alternatives for each contiguous tail token
         toks_alts = [*takewhile(lambda exts: len(exts) > 1, tail_toks_extensions)]
-
         return toks_alts
 
-    def regen_toks(self, ids: IntTensor, toks_alts: list[list[int]]) -> IntTensor:
+    def regenerate_tokens(self, ids: IntTensor, toks_alts: list[list[int]]) -> IntTensor:
         past_kv = None
-        for alts in reversed(toks_alts): # regenerate last trimmed toks first
+        for tok_alts in reversed(toks_alts): # regenerate last trimmed toks first
             ids = self.model.greedy_search(
                 ids,
-                logits_processor=AllowedToks(lambda *_, toks=alts: toks, 1),
+                force_words_ids=[[tok_alts]],
                 stopping_criteria=self.max_length_1,
                 pad_token_id=self.model.config.pad_token_id,
                 return_dict_in_generate=self.use_cache,
